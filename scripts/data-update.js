0e44c1e1838b119c13f9e222e278523fc55b5c18
Tim Carry <tim@pixelastic.com>
2020-11-28 15:22:55 +0100

    test(record): Add some basic test on records

--- scripts/data-update.js
+++ scripts/data-update.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const helper = require('../lib/main.js');
 const pMap = require('golgoth/lib/pMap');
-const writeJson = require('firost/lib/writeJson');
+const writeJson = require('firost/writeJson');
 
 (async () => {
   helper.init();
   const gods = await helper.allNames();
-  await pMap(gods, async (godName) => {
-    const record = await helper.record(godName);
-    const { slug } = record;
-    const filepath = path.resolve(`./data/${slug}.json`);
-    await writeJson(record, filepath);
-  });
+  await pMap(
+    gods,
+    async (godName, index) => {
+      try {
+        const record = await helper.record(godName);
+        const { slug } = record;
+        const filepath = path.resolve(`./data/${slug}.json`);
+        await writeJson(record, filepath);
+      } catch (err) {
+        console.info(err);
+        console.info({ index, godName });
+        process.exit(0);
+      }
+    },
+    { concurrency: 1 }
+  );
 })();
