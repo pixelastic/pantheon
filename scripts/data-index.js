ef46bb5056c8303b9e0ab747279dcceea1549fe4
Tim Carry <tim@pixelastic.com>
2020-11-28 15:51:10 +0100

    Add url

---
+++ scripts/data-index.js
@@ -0,0 +1,39 @@
+const indexing = require('algolia-indexing');
+const readJson = require('firost/readJson');
+const glob = require('firost/glob');
+const consoleError = require('firost/consoleError');
+const pMap = require('golgoth/lib/pMap');
+const config = require('../src/_data/config.js');
+const _ = require('golgoth/lib/lodash');
+
+(async function () {
+  const credentials = {
+    appId: config.algolia.appId,
+    apiKey: process.env.ALGOLIA_API_KEY,
+    indexName: config.algolia.indexName,
+  };
+  const settings = {
+    searchableAttributes: [
+      'name',
+      'unordered(description)',
+      'unordered(titles)',
+    ],
+    customRanking: [],
+    attributesForFaceting: ['alignment', 'domains'],
+    attributesToSnippet: ['description'],
+  };
+
+  indexing.verbose();
+  indexing.config({
+    batchMaxSize: 100,
+  });
+
+  try {
+    const files = await glob('./data/*.json');
+    const records = _.flatten(await pMap(files, readJson));
+    await indexing.fullAtomic(credentials, records, settings);
+  } catch (err) {
+    consoleError(err.message);
+    process.exit(1);
+  }
+})();
