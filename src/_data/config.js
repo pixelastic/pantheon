906c68d527ef94d022149327a9bbc78713de3fc6
Tim Carry <tim@pixelastic.com>
2020-12-02 22:55:14 +0100

    fix(pug): Remove debug statement

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   algolia: {
     appId: 'O3F8QXYK6R',
-    indexName: 'gamemaster_gods',
     apiKey: '62e71c961e169577a037176652496736',
+    indexName: 'gamemaster_gods',
   },
 };
