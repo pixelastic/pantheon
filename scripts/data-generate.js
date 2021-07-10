79ced667b517ceb415d6cf40aaca41d02408e1f8
Tim Carry <tim@pixelastic.com>
2021-07-10 23:29:12 +0200

    Add jpg pictures

--- scripts/data-generate.js
+++ scripts/data-generate.js
@@ -1,20 +1,21 @@
 const path = require('path');
 const helper = require('../lib/main.js');
 const pMap = require('golgoth/pMap');
-const writeJson = require('firost/writeJson');
+const { writeJson, spinner } = require('firost');
 
 (async () => {
   helper.init();
   const gods = await helper.allNames();
+  const progress = spinner(gods.length);
   await pMap(
     gods,
     async (godName, index) => {
       try {
         const record = await helper.record(godName);
-        const { slug, picture } = record;
+        const { slug, name } = record;
 
         // Download the picture in ./pictures, with the right extension
-        await helper.download(picture);
+        await helper.download(record);
         // const extname = path.extname(picture)
         // console.info({ extname })
         // Also, remove the background
@@ -24,12 +25,15 @@ const writeJson = require('firost/writeJson');
 
         const filepath = path.resolve(`./data/${slug}.json`);
         await writeJson(record, filepath);
+        progress.tick(name);
       } catch (err) {
+        progress.error();
         console.info(err);
         console.info({ index, godName });
         process.exit(0);
       }
     },
-    { concurrency: 1 }
+    { concurrency: 25 }
   );
+  progress.success();
 })();
