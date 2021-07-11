26d72d4006a86a791fddaa3a534208e5e8381370
Tim Carry <tim@pixelastic.com>
2021-07-11 23:57:14 +0200

    fix: #23 rank gods with picture higher

--- scripts/data-generate.js
+++ scripts/data-generate.js
@@ -2,7 +2,6 @@ const path = require('path');
 const helper = require('../lib/main.js');
 const pMap = require('golgoth/pMap');
 const { writeJson, spinner } = require('firost');
-const imoen = require('imoen');
 
 (async () => {
   helper.init();
@@ -14,16 +13,6 @@ const imoen = require('imoen');
       try {
         const record = await helper.record(godName);
         const { slug, name } = record;
-
-        // Update picture with metadata
-        const picturePath = path.resolve('./src/pictures', `${slug}.png`);
-        const { width, height, lqip, hash } = await imoen(picturePath);
-        record.picture = {
-          lqip,
-          width,
-          height,
-          hash,
-        };
 
         const filepath = path.resolve(`./data/${slug}.json`);
         await writeJson(record, filepath);
