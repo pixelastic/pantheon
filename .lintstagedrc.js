7aff5fc81fff64f832cbb4de388426d42aa92be4
Tim Carry <tim@pixelastic.com>
2021-07-15 17:13:53 +0200

    Test updating pictures

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,4 @@
 const config = require('aberlaas/lib/configs/lintstaged.js');
-console.info(config);
 module.exports = {
   ...config,
   'src/pictures/*.png': ['yarn run data:pictures', 'git add ./data'],
