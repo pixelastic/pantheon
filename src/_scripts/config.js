e087899995797a72681d4c4b9b3c7bbfa3b3de13
Tim Carry <tim@pixelastic.com>
2021-07-15 15:45:30 +0200

    Add filtering out some pantheons

--- src/_scripts/config.js
+++ src/_scripts/config.js
@@ -1,4 +1,5 @@
 const transforms = require('./transforms');
+let algoliaHelper = null;
 
 module.exports = {
   credentials: {
@@ -8,7 +9,11 @@ module.exports = {
   },
   hitName: 'god',
   placeholder: 'Search for any name, domain or title',
-  // See documentation below for the widgets format
+  onSearch(query, helper) {
+    // Make the helper publicly accessible because we need it in the
+    // transformItems
+    algoliaHelper = helper;
+  },
   widgets: [
     {
       title: 'Alignment',
@@ -16,10 +21,51 @@ module.exports = {
         attribute: 'alignment',
       },
     },
+    // Pantheons is an inverted refinements. Selecting an item removes the
+    // matching hits from the list. To achieve that, we hack around the default
+    // RefinementList by adding a "-" at the start of each facet value, to
+    // exclude them. We also set it to conjunctive (AND) and update the CSS
+    // classes to invert the display, to make it look like everything is
+    // selected on load.
+    // Finally, we need to reconstruct the list manually by calling the helper
+    // to add the list of current refinements, as they are no longer returned in
+    // the widget
     {
       title: 'Pantheons',
       options: {
         attribute: 'pantheons',
+        operator: 'and',
+        limit: 100,
+        cssClasses: {
+          root: 'ais-InvertedRefinementList',
+        },
+        transformItems(items) {
+          const updatedItems = [];
+          const currentRefinements = algoliaHelper.getRefinements('pantheons');
+
+          // First, add all the current refinements
+          currentRefinements.forEach((currentRefinement) => {
+            const { value } = currentRefinement;
+            const label = value.replace(/^-/, '');
+            updatedItems.push({
+              value,
+              label,
+              highlighted: label,
+              isRefined: true,
+            });
+          });
+
+          // Then, all the items, but negating their value
+          items.forEach((item) => {
+            const { value } = item;
+            updatedItems.push({
+              ...item,
+              value: `-${value}`,
+            });
+          });
+
+          return updatedItems;
+        },
       },
     },
   ],
